ebb36698f9a23a0785b33c653cf991cc766c8c54
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-25 17:52:45 +0300

    Match the Same String Twice with Backreferences in Regular Expressions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var str = `This history is his, it is`;
-var regex = /\Bis\B/g
+var str = `<b>Bold</b><i>italics</i>`;
+var regex = /<(\w+)>(.*)<\/\1>/g;
+
+console.log(str.replace(regex, '$2\n')) || displayInPreview(str.replace(regex, '$2\n'));
 
 /**
  * @param  String str
@@ -15,3 +17,13 @@ const output = (str, regex, target) => {
     );
 }
 output(str, regex, document.querySelector('pre'));
+
+
+
+// display in plunker preview
+function displayInPreview(string) {
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode(string); 
+  newDiv.appendChild(newContent);
+  document.body.appendChild(newDiv)
+}
